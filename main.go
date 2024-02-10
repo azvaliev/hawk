7756f8f7e3cedf8219b116bdd805206a2f1a3666
Azat Valiev <azat@valievventures.com>
2024-02-10 17:39:54 -0600

    error logging

--- main.go
+++ main.go
@@ -13,22 +13,23 @@ import (
 
 func main() {
 	args := cli.MustParseCLIArgs()
+	errLog := log.New(os.Stderr, "", 0)
 
 	fullPath, resolvePathErr := resolvepath.GetDirAbsolutePath(args.Dir)
 	if resolvePathErr != nil {
-		log.Fatalf("Failed to resolve path \"%s\": %s\n", args.Dir, resolvePathErr.Context)
+		errLog.Fatalf("Failed to resolve path \"%s\": %s\n", args.Dir, resolvePathErr.Context)
 	}
 	fileServer := httpfs.GetHTTPFileServer(os.DirFS(fullPath))
 
 	listener, err := net.Listen("tcp", fmt.Sprintf(":%d", args.Port))
 	if err != nil {
-		log.Fatalf("Failed to start server: %s", err)
+		errLog.Fatalf("Failed to start server: %s", err)
 	}
 	port := listener.Addr().(*net.TCPAddr).Port
 	fmt.Printf("Listening on http://localhost:%d\n", port)
 
 	err = http.Serve(listener, fileServer)
 	if err != nil {
-		log.Fatalf("Fatal Error: %s\n", err)
+		errLog.Fatalf("Fatal Error: %s\n", err)
 	}
 }
